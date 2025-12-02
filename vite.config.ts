e1a21957dcfc0919783522f845df5ee9506bddbd
brendan.cohen <tools@greychair.io>
2025-12-01 21:16:13 -0500

    Fix merge conflicts in most components - ShippingReturnsPage still needs work

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      },
-    },
     outDir: 'dist',
     assetsDir: 'assets',
   },
